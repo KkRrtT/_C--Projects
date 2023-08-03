c9a0d98a71bd5f6b61481b0005e8e3e49ea9b77f
KkRrtT <wolfstein546@gmail.com>
2023-08-03 17:51:26 +0200

    Created Repository and project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        
+        static void Main()
+        {
+            string name = "1eee";
+            Console.WriteLine(name.IndexOf("e"));
+        }
+    }   
+}
